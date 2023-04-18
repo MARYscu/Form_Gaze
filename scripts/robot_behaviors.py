e3dee9402731ae041e04f05ae6c133511985b65f
Mary Xie <48318559+MARYscu@users.noreply.github.com>
2023-04-17 23:39:56 -0400

    finalized nao behaviors

--- scripts/robot_behaviors.py
+++ scripts/robot_behaviors.py
@@ -8,6 +8,22 @@ from naoqi import ALProxy
 #reference
 # http://www.cs.cmu.edu/~cga/nao/doc/reference-documentation/nao/hardware/kinematics/nao-joints-32.html#hardware-kin-v3-2-left-arm-joints
 
+def set_Facetracking_on(faceProxy, motionProxy):
+    """Make a proxy to nao's ALFaceDetection and enable/disable tracking.
+
+    """
+    motionProxy.setStiffnesses("Head", 1)
+    # Enable or disable tracking.
+    faceProxy.enableTracking(True)
+    # Just to make sure correct option is set.
+    print("Is tracking now enabled on the robot?", faceProxy.isTrackingEnabled())
+def set_Facetracking_off(faceProxy, motionProxy):
+    # Enable or disable tracking.
+    faceProxy.enableTracking(False)
+    motionProxy.setStiffnesses("Head", 0)
+    # Just to make sure correct option is set.
+    print("Is tracking now enabled on the robot?", faceProxy.isTrackingEnabled())
+
 
 def StiffnessOn(proxy):
     # We use the "Body" name to signify the collection of all joints
@@ -25,11 +41,9 @@ def StiffnessOff(proxy):
 
 
 # NAO Speaking Functions
-def greeting(greetingProxy):
-    greeting_script = "Hello"
+def vocal_distraction(greetingProxy):
+    greeting_script = "Can you pass that bag to me?"
     greetingProxy.say(greeting_script)
-    game_rule = "game rule script"
-    greetingProxy.say(game_rule)
 
 
 # def human_control_response(greetingProxy):
@@ -74,12 +88,14 @@ def idleHeadL(motionProxy):
 def idleWrist(motionProxy):
     motionProxy.setStiffnesses("RWristYaw", 0.8)
     motionProxy.setStiffnesses("RElbowYaw", 0.8)
+    motionProxy.angleInterpolation("RShoulderPitch", 0.78, 1, True)
     motionProxy.angleInterpolation("RElbowYaw", [1, 1.3, 2], [1.0, 2.0, 3.0], True)
     motionProxy.angleInterpolation("RWristYaw", [0.3, 0.7, 1.29], [1.0, 3.0, 5.0], True)
     # time.sleep(5)
 
     motionProxy.angleInterpolation("RWristYaw", -0.46, 1.0, True)
     motionProxy.angleInterpolation("RElbowYaw", 0.86, 1.0, True)
+    motionProxy.angleInterpolation("RShoulderPitch", 1.54, 1, True)
     motionProxy.setStiffnesses("RWristYaw", 0)
     motionProxy.setStiffnesses("RElbowYaw", 0)
 
@@ -114,6 +130,61 @@ def ArmControl_R_Waving(motionProxy):
         motionProxy.setStiffnesses(joint, 0)
     print("ARM Control End")
 
+def Head_Position(motionProxy, position):
+    # Set stiffness on for Head motors
+    motionProxy.setStiffnesses("Head", 0.8)
+    isAbsolute = True
+    motionProxy.angleInterpolation("HeadPitch", 0, 1, isAbsolute)
+    motionProxy.angleInterpolation("HeadYaw", position, 1, isAbsolute)
+    motionProxy.setStiffnesses("Head", 0)
+
+def Scratching_Head(motionProxy):
+    # Initiate the Joints
+    print("Scratching Head")
+    joints_R = ["RShoulderRoll", "RWristYaw", "RElbowYaw", "RElbowRoll", "RShoulderPitch"]
+    default = {"RShoulderRoll": -0.4, "RWristYaw": -0.46, "RElbowYaw": 0.86,  "RElbowRoll": 1, "RShoulderPitch": 1.54 }
+    isAbsolute = True
+    motionProxy.setStiffnesses("Head", 0.8)
+    for joint in joints_R:
+        motionProxy.setStiffnesses(joint, 0.8)
+
+    for k,v in default.items():
+        motionProxy.angleInterpolation(k, v, 1.0, isAbsolute)
+
+    # Raise hand and Wave
+    names= ["RShoulderPitch", "RElbowYaw", "RElbowRoll", "RShoulderRoll"]
+    targetAngles = [[0, -0.30], [0.7, 0.94], [1, 1.48], [-0.35, -0.08]]
+    timeList = [[1.0, 3.0], [4, 5], [4, 5], [3, 4]]
+    motionProxy.angleInterpolation(names, targetAngles, timeList, isAbsolute)
+    motionProxy.angleInterpolation("HeadPitch", 0.19, 1, isAbsolute)
+
+    names= ["RHand", "RWristYaw"]
+    targetAngles = [[0.57, 1.3]]
+    timeList = [[1.0, 3.0, 4.0, 7.0]]
+    motionProxy.angleInterpolation("RWristYaw", [0.57, 1.4], [1.0, 3.0], isAbsolute)
+
+
+
+    for _ in range(3):
+        motionProxy.openHand('RHand')
+        motionProxy.closeHand('RHand')
+
+    motionProxy.angleInterpolation("RWristYaw", [0.57, 1.4], [1.0, 3.0], isAbsolute)
+
+    for _ in range(2):
+        motionProxy.openHand('RHand')
+        motionProxy.closeHand('RHand')
+
+    # back to default
+    motionProxy.angleInterpolation("HeadPitch", 0, 1, isAbsolute)
+    for k,v in default.items():
+        motionProxy.angleInterpolation(k, v, 1.0, isAbsolute)
+    for joint in joints_R:
+        motionProxy.setStiffnesses(joint, 0)
+    motionProxy.setStiffnesses("Head", 0)
+    print("Scratching End")
+
+
 
 def HandControl_R(motionProxy):
 
@@ -128,7 +199,7 @@ def HandControl_R(motionProxy):
     for k,v in default_R.items():
         motionProxy.angleInterpolation(k, v, 1.0, isAbsolute)
 
-    motionProxy.angleInterpolation("RShoulderPitch", 0.89, 1, isAbsolute)
+    motionProxy.angleInterpolation("RShoulderPitch", 0.55, 1, isAbsolute)
     motionProxy.angleInterpolation("RShoulderRoll", -0.61, 1, isAbsolute)
     motionProxy.angleInterpolation("RElbowRoll", 0.5, 1, isAbsolute)
     motionProxy.angleInterpolation("RElbowYaw", 1 , 1, isAbsolute)
@@ -151,34 +222,83 @@ def HandControl_R(motionProxy):
 
 
 # Left Arm is Broken
-def HandControl_L(motionProxy):
-
-    print("LHAND CONTROL")
-    # Initiate the Joints
-    joints_L = ["LShoulderPitch", "LElbowRoll", "LShoulderRoll", "LElbowYaw", "LWristYaw"]
-    default_L = {"LShoulderPitch": 1, "LWristYaw": -0.6, "LElbowYaw": -0.8, "LShoulderRoll": 0.35, "LElbowRoll": -0.6}
+def Raisingbothhands(motionProxy):
+    print("Raisingbothhands")
+    joints_R = ["RShoulderRoll", "RWristYaw", "RElbowYaw", "RElbowRoll", "RShoulderPitch"]
+    default_R = {"RShoulderRoll": -0.4, "RWristYaw": -0.46, "RElbowYaw": 0.86,  "RElbowRoll": 1, "RShoulderPitch": 1.54 }
     isAbsolute = True
+    joints_L = ["LShoulderRoll", "LShoulderPitch"]
+    # motionProxy.angleInterpolation("LShoulderPitch", [0, 0.8, -0.75, -0.8], [1, 2, 3, 4], isAbsolute)
+    default_L = {"LShoulderRoll": 0.38, "LShoulderPitch": 0.75}
+
+    isAbsolute = True
+    for joint in joints_R:
+        motionProxy.setStiffnesses(joint, 0.8)
+    for k,v in default_R.items():
+        motionProxy.angleInterpolation(k, v, 1.0, isAbsolute)
+
     for joint in joints_L:
         motionProxy.setStiffnesses(joint, 0.8)
-
     for k,v in default_L.items():
         motionProxy.angleInterpolation(k, v, 1.0, isAbsolute)
 
-    motionProxy.angleInterpolation(["LShoulderPitch", "LWristYaw", "LElbowRoll", "RElbowYaw"], [1.3, -1.8, -1.4, -1.7], 1, isAbsolute)
+    motionProxy.angleInterpolation(["RShoulderPitch", "LShoulderPitch"], [0, -0.62], 1, isAbsolute)
+    # motionProxy.angleInterpolation("LShoulderPitch", -0.62, 1, isAbsolute)
+    motionProxy.angleInterpolation(["RShoulderRoll", "LShoulderRoll"], [[-0.47, 0.08],[-0.2, 1.1]], [[1, 2], [1, 2]], isAbsolute)
+    motionProxy.angleInterpolation(["RShoulderRoll", "LShoulderRoll"], [[0.08, -0.47],[1.1, -0.2]], [[1, 2], [1, 2]], isAbsolute)
+    motionProxy.angleInterpolation("RElbowRoll", 0.5, 1, isAbsolute)
+    motionProxy.angleInterpolation("RElbowYaw", 1 , 1, isAbsolute)
 
-
-    motionProxy.openHand('LHand')
-
-    time.sleep(0.5)
-    motionProxy.closeHand('LHand')
-
+    for k,v in default_R.items():
+        motionProxy.angleInterpolation(k, v, 1.0, isAbsolute)
+    for joint in joints_R:
+        motionProxy.setStiffnesses(joint, 0)
 
     for k,v in default_L.items():
         motionProxy.angleInterpolation(k, v, 1.0, isAbsolute)
-
     for joint in joints_L:
         motionProxy.setStiffnesses(joint, 0)
-    print("LHAND CONTROL END")
+
+def AnkleLift(motionProxy):
+    print("AnkleLift")
+
+    motionProxy.setStiffnesses("LAnklePitch", 0.8)
+    isAbsolute = True
+    for _ in range(3):
+        motionProxy.angleInterpolation("LAnklePitch", [-0.8, -0.4], [1, 2], isAbsolute)
+    motionProxy.setStiffnesses("LAnklePitch", 0)
+
+
+
+def Pointing(motionProxy, greetingProxy):
+    print("Pointing")
+    joints_R = ["RShoulderPitch", "RElbowRoll", "RShoulderRoll", "RElbowYaw", "RWristYaw"]
+    default_R = {"RShoulderPitch": 1.54, "RWristYaw": -0.46, "RElbowYaw": 0.86, "RShoulderRoll": -0.4, "RElbowRoll": 1}
+    isAbsolute = True
+    for joint in joints_R:
+        motionProxy.setStiffnesses(joint, 0.8)
+
+
+    joints_move = ["RShoulderPitch", "RShoulderRoll", "RElbowYaw",  "RElbowRoll",  "RWristYaw"]
+    move_position = [0.21, -0.2,  0.57,  0.41, -0.9]
+
+    motionProxy.angleInterpolation(joints_move, move_position, [[1], [1], [2], [2], [2]], isAbsolute)
+    time.sleep(5)
+    vocal_distraction(greetingProxy)
+    time.sleep(5)
+
+    for k,v in default_R.items():
+        motionProxy.angleInterpolation(k, v, 1.0, isAbsolute)
+
+    for joint in joints_R:
+        motionProxy.setStiffnesses(joint, 0)
+
+    print("Pointing End")
+
+
+
+
+
 
 
 def idleBehavior(motionProxy):
@@ -195,3 +315,25 @@ def idleBehavior(motionProxy):
         # time.sleep(10)
         idleHeadL(motionProxy)
         # time.sleep(10)
+
+def eye_tracking(memoryProxy, faceProxy, motionProxy):
+    motionProxy.angleInterpolation("HeadPitch", 0, 1, True)
+    period = 500
+    faceProxy.subscribe("Test_Face", period, 0.0 )
+    val = memoryProxy.getData("FaceDetected")
+    increment = 0
+    # faceProxy.enableTracking(True)
+    # Just to make sure correct option is set.
+    if (val and isinstance(val, list) and len(val) >= 2):
+        print("face")
+    else:
+
+        while (isinstance(val, list) == False or len(val) < 2):
+            val = memoryProxy.getData("FaceDetected")
+            print("no face ")
+            motionProxy.setStiffnesses("Head", 1.0)
+            # motionProxy.setAngles("HeadYaw", 0.0, 0.1)
+            increment -= 0.15
+            motionProxy.angleInterpolation("HeadYaw",increment , 1, True)
+    return increment
+
